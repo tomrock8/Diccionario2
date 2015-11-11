539c91dfa84e15c72e7b317c6f1d7bd668dc0970
tomrock8 <tomrock8@gmail.com>
2015-11-11 19:41:05 +0100

    Corregida prueba 1

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,36 @@
 public class Program {
 
-	public static void main(String[] args) {
-		Diccionario diccio = new DiccMiLista();
-		if (args.length >= 1) {
-			diccio.leeDiccionario(args[0]);
-			diccio.visualiza();
-		} else
-			System.out.println("Forma uso: java p05 p05.dic");
-
-	}
-
+    public static void main(String[] args) {
+        String lexeme = new String("quiet");
+        Palabra2 word = new Palabra2(lexeme, null);
+        String[] meaning = new String[4];
+        char[] language = new char[4];
+        meaning[0] = new String("tranquilo");
+        language[0] = 'E';
+        meaning[1] = new String("calme");
+        language[1] = 'F';
+        meaning[2] = new String("quieto");
+        language[2] = 'P';
+        meaning[3] = new String("ancora");
+        language[3] = 'I';
+        int pos = -1;
+        for (int i = 0; i < language.length; i++) {
+            pos = word.setTrad(meaning[i], language[i]);
+            System.out.println("traduccion a " + language[i] + "; acepcion " + meaning[i] + " -> " + pos);
+        }
+        word.escribeInfo();
+        meaning[0] = new String("tranquilo");
+        language[0] = 'P';
+        meaning[1] = new String("tranquillo");
+        language[1] = 'I';
+        meaning[2] = new String("calme");
+        language[2] = 'F';
+        meaning[3] = new String("callado");
+        language[3] = 'E';
+        for (int i = 0; i < language.length; i++) {
+            pos = word.setTrad(meaning[i], language[i]);
+            System.out.println("traduccion a " + language[i] + "; acepcion " + meaning[i] + " -> " + pos);
+        }
+        word.escribeInfo();
+    }
 }
